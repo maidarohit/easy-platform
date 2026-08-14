77c05857ab1f2ecab2706c90d27634b3ef3df7dc
Rohit Maida <maidarohit94@gmail.com>
2026-08-15 00:13:00 +0530

    Fix Firebase production config

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmdSKer4OXM6s7EYjY1JXSUb5BwaRowfA",
+  apiKey: "AIzaSyBmdSKer40XM6s7EYjYlJXSUb5BwaRowfA",
   authDomain: "easy-platform-b757b.firebaseapp.com",
   projectId: "easy-platform-b757b",
   storageBucket: "easy-platform-b757b.firebasestorage.app",
@@ -9,8 +9,6 @@ const firebaseConfig = {
   appId: "1:143628261298:web:6b3f12227bdacc17ecb6ab",
 };
 
-const app = !getApps().length
-  ? initializeApp(firebaseConfig)
-  : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export default app;
