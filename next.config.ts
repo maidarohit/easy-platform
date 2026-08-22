ec252c9c99c3aa350a6a9906030900e4f471302f
Rohit Maida <maidarohit94@gmail.com>
2026-08-23 02:19:38 +0530

    Add CSP report-only policy

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,11 @@ const nextConfig: NextConfig = {
             value:
               "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(self), payment=(), usb=()",
           },
+          {
+            key: "Content-Security-Policy-Report-Only",
+            value:
+              "default-src 'self'; base-uri 'self'; object-src 'none'; script-src 'self' 'unsafe-inline'; script-src-attr 'none'; style-src 'self' 'unsafe-inline'; img-src 'self' blob:; font-src 'self'; connect-src 'self' blob: https://identitytoolkit.googleapis.com https://securetoken.googleapis.com; media-src 'self' blob:; frame-src 'none'; frame-ancestors 'none'; form-action 'self'; manifest-src 'self'; upgrade-insecure-requests",
+          },
         ],
       },
     ];
